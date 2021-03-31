c5ea430f9563b29f5a1bf3244ee82c090cfa8779
geoffrey <geoffreylao@outlook.com>
2021-03-31 00:07:43 -0300

    file upload to mongo db atlas

---
+++ mern-multiple-upload/react-multiple-files-upload/backend/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const api = require('../backend/routes/user.routes')
+
+require('dotenv').config();
+
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(cors());
+
+app.use('/public', express.static('public'));
+
+app.use('/api', api)
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log('Connected to port ' + port)
+})
+
+
+
+app.use((req, res, next) => {
+    // Error goes via `next()` method
+    setImmediate(() => {
+        next(new Error('Something went wrong'));
+    });
+});
+
+app.use(function (err, req, res, next) {
+    console.error(err.message);
+    if (!err.statusCode) err.statusCode = 500;
+    res.status(err.statusCode).send(err.message);
+});
