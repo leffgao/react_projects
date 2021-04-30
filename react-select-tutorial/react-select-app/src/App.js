ab28b88e6e781ac722aaff8c334f3d219944b7b4
geoffrey <geoffreylao@outlook.com>
2021-04-30 16:44:52 -0300

    react select with melee stock icons

--- react-select-tutorial/react-select-app/src/App.js
+++ react-select-tutorial/react-select-app/src/App.js
@@ -13,13 +13,37 @@ export default class App extends Component {
   }
 
  async getOptions(){
-    const res = await axios.get('https://jsonplaceholder.typicode.com/users')
-    const data = res.data
+    // const res = await axios.get('https://jsonplaceholder.typicode.com/users')
+    // const data = res.data
 
-    const options = data.map(d => ({
-      "value" : d.name,
-      "label" : <div><img src={'logo512.png'} height="30px" width="30px"/> {d.name}</div>
+    // const options = data.map(d => ({
+    //   "value" : d.name,
+    //   "label" : <div><img src={'logo512.png'} height="30px" width="30px"/> {d.name}</div>
 
+    // }))
+
+    let chararr = [
+      "BOWSER","CAPTAIN_FALCON","DONKEY_KONG","DR_MARIO",
+      "FALCO","FOX","GAME_AND_WATCH","GANONDORF","ICE_CLIMBERS",
+      "JIGGLYPUFF","KIRBY","LINK","LUIGI","MARIO","MARTH","MEWTWO",
+      "NESS","PEACH","PICHU","PIKACHU","ROY","SAMUS","SHEIK","YOSHI",
+      "YOUNG_LINK","ZELDA"
+    ]
+
+    let charpng = [
+      "Bowser.png","Captain Falcon.png","Donkey Kong.png","Dr. Mario.png",
+      "Falco.png","Fox.png","Game & Watch.png","Ganondorf.png","Ice Climbers.png",
+      "Jigglypuff.png","Kirby.png","Link.png","Luigi.png","Mario.png","Marth.png","Mewtwo.png",
+      "Ness.png","Peach.png","Pichu.png","Pikachu.png","Roy.png","Samus.png","Sheik.png","Yoshi.png",
+      "Young Link.png","Zelda.png"
+    ]
+
+    const arr1 = ["1","2"];
+    const arr2 = ["one", "two"];
+
+    const options = chararr.map((x,i) => ({
+      "value": x,
+      "label": <div><img src={`stock_icons/${charpng[i]}`} height="30px" width="30px"/> {x}</div>
     }))
 
     this.setState({selectOptions: options})
